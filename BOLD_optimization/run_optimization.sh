9bf128b7e77a5bda3662f62d32f2e498285047c1
Oliver Maith <oli_maith@gmx.de>
2025-12-19 15:27:52 +0100

    BOLD optimizaton: made optimization runs parallel

--- BOLD_optimization/run_optimization.sh
+++ BOLD_optimization/run_optimization.sh
@@ -1,8 +1,10 @@
 # create data folder
 python create_data_folder.py
 # Run multiple deap cma optimizations which by themselves run multiple simulations in parallel (13) -> n*13 cores needed
-python deap_cma_opt.py --dbs off --optimization-run 1
-python deap_cma_opt.py --dbs off --optimization-run 2
+python deap_cma_opt.py --dbs off --optimization-run 1 &
+python deap_cma_opt.py --dbs off --optimization-run 2 &
+wait
 # After dbs off optimizations run dbs on optimizations
-python deap_cma_opt.py --dbs on --optimization-run 1
-python deap_cma_opt.py --dbs on --optimization-run 2
+python deap_cma_opt.py --dbs on --optimization-run 1 &
+python deap_cma_opt.py --dbs on --optimization-run 2 &
+wait
